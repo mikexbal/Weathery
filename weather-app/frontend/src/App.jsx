f90b01bbfc9c1104733180daa8a6a71d6b1db87a
Mike <mxbalogun@gmail.com>
2025-05-24 14:37:16 -0400

    Passed data from searchbar to display componet

--- weather-app/frontend/src/App.jsx
+++ weather-app/frontend/src/App.jsx
@@ -2,16 +2,22 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-import SearchBar from "./components/SearchBar/Search";
+import SearchBar from "./components/SearchComponent/Search";
+import WeatherDisplay from "./components/WeatherComponent/Display";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [data, setData] = useState(null);
+
+  const handleSearch = (json) => {
+    setData(json);
+  };
 
   return (
     <>
       <div id="container">
         <h1 id="heading">Enter location</h1>
-        <SearchBar />
+        <SearchBar onSearch={setData} />
+        <WeatherDisplay data={data} />
       </div>
     </>
   );
