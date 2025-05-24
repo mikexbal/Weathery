d7447247b339af4ee4a65b7ee2b534db46830e3e
Mike <mxbalogun@gmail.com>
2025-05-24 15:09:01 -0400

    Removed unecessary imports

--- weather-app/frontend/src/App.jsx
+++ weather-app/frontend/src/App.jsx
@@ -1,6 +1,4 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 import SearchBar from "./components/SearchComponent/Search";
 import WeatherDisplay from "./components/WeatherComponent/Display";
